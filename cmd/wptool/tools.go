33999713b044158aebefe2ea14b2c7f3ea66add2
Jennifer <JenniferMack@users.noreply.github.com>
2019-05-16 15:47:58 -0700

    add extracted method mergeAndSave

--- cmd/wptool/tools.go
+++ cmd/wptool/tools.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"os"
+
+	"repo.local/wputil"
 )
 
 func errs(e error, m string) {
 	if e != nil {
+		log.SetPrefix("[   error] ")
 		log.Fatalf("%s: %s", m, e)
 	}
 }
@@ -21,6 +26,34 @@ func openFileR(s, m string) *os.File {
 		log.Fatalf("%s: %s", m, err)
 	}
 	return f
+}
+
+func mergeAndSave(f wputil.Feed, p string) (int, int, error) {
+	b, err := ioutil.ReadFile(p)
+	if err != nil {
+		log.Printf("%s does not exist, skipping", p)
+	}
+
+	_, err = f.Write(b)
+	if err != nil {
+		return 0, 0, fmt.Errorf("load existing: %s", err)
+	}
+
+	saved, err := os.Create(p)
+	if err != nil {
+		return 0, 0, fmt.Errorf("file reset: %s", err)
+	}
+	defer saved.Close()
+
+	n, err := io.Copy(saved, &f)
+	if err != nil {
+		return 0, 0, fmt.Errorf("json write: %s", err)
+	}
+	err = saved.Close()
+	if err != nil {
+		return 0, 0, fmt.Errorf("json save: %s", err)
+	}
+	return int(n), f.Len(), nil
 }
 
 func size(b int) string {
