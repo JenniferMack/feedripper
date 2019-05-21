0e97766eb33ea3a717cd184d41d326f12ffb4170
Jennifer <JenniferMack@users.noreply.github.com>
2019-05-21 15:18:25 -0700

    working, with filename input

--- wpfeed/cmd/feed-utils/print.go
+++ wpfeed/cmd/feed-utils/print.go
@@ -3,24 +3,30 @@ package main
 import (
 	"bytes"
 	"fmt"
-	"io"
+	"os"
 
 	"golang.org/x/net/html"
 )
 
-func printHeadings(in io.Reader) []byte {
+func printHeadings(feed string) ([]byte, error) {
+	in, err := os.Open(feed)
+	if err != nil {
+		return nil, err
+	}
+	defer in.Close()
+
 	var cnt int
 	out := bytes.Buffer{}
 
 	h := func(n *html.Node) {
 		if n.Type == html.ElementNode && n.Data == "h1" {
-			fmt.Fprintf(&out, n.FirstChild.Data)
+			fmt.Fprintf(&out, "-- %s --\n", n.FirstChild.Data)
 			cnt = 1
 			return
 		}
 		if n.Type == html.ElementNode && n.Data == "h2" {
 			f := n.FirstChild.NextSibling
-			fmt.Fprintf(&out, "%02d  - %s\n", cnt, f.FirstChild.Data)
+			fmt.Fprintf(&out, "%02d - %s\n", cnt, f.FirstChild.Data)
 			cnt++
 			return
 		}
@@ -28,11 +34,12 @@ func printHeadings(in io.Reader) []byte {
 
 	doc, err := html.Parse(in)
 	if err != nil {
-		return nil
+		return nil, err
 	}
+	in.Close()
 
 	parseHTML(doc, h)
-	return out.Bytes()
+	return out.Bytes(), nil
 }
 
 func parseHTML(n *html.Node, before func(n *html.Node)) {
