0a216283094ecb2e0fbee7cc82a0a7cfaabded3a
Jennifer <JenniferMack@users.noreply.github.com>
2019-05-21 12:22:10 -0700

    add tests for work dir

--- wpfeed/conf_test.go
+++ wpfeed/conf_test.go
@@ -3,7 +3,18 @@ package wpfeed
 import "testing"
 
 func TestWorkDir(t *testing.T) {
-	c := Config{}
+	c := Config{
+		WorkDir: "foo",
+	}
+	// mock os.PWD
+	pwd := "/home/user/feed/foo"
+	if !c.IsWorkDir(pwd, nil) {
+		t.Error("in work dir")
+	}
+	pwd = "/home/user/feed/bar"
+	if c.IsWorkDir(pwd, nil) {
+		t.Error("not in work dir")
+	}
 }
 
 func TestOORange(t *testing.T) {
