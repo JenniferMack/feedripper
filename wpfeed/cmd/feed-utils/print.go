69f5b4792cd8f7558b075a0dfd3d1fc024c42688
Jennifer <JenniferMack@users.noreply.github.com>
2019-05-21 14:55:41 -0700

    add returns and io.Writer use

--- wpfeed/cmd/feed-utils/print.go
+++ wpfeed/cmd/feed-utils/print.go
@@ -1,45 +1,46 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
-	"os"
+	"io"
 
 	"golang.org/x/net/html"
 )
 
-func printHeadings() {
+func printHeadings(in io.Reader) []byte {
 	var cnt int
+	out := bytes.Buffer{}
+
 	h := func(n *html.Node) {
 		if n.Type == html.ElementNode && n.Data == "h1" {
-			fmt.Println(n.FirstChild.Data)
+			fmt.Fprintf(&out, n.FirstChild.Data)
 			cnt = 1
 			return
 		}
 		if n.Type == html.ElementNode && n.Data == "h2" {
 			f := n.FirstChild.NextSibling
-			fmt.Printf("%02d  - %s\n", cnt, f.FirstChild.Data)
+			fmt.Fprintf(&out, "%02d  - %s\n", cnt, f.FirstChild.Data)
 			cnt++
 			return
 		}
 	}
 
-	doc, err := html.Parse(os.Stdin)
+	doc, err := html.Parse(in)
 	if err != nil {
-		return
+		return nil
 	}
-	parseHTML(doc, h, nil)
+
+	parseHTML(doc, h)
+	return out.Bytes()
 }
 
-func parseHTML(n *html.Node, before, after func(n *html.Node)) {
+func parseHTML(n *html.Node, before func(n *html.Node)) {
 	if before != nil {
 		before(n)
 	}
 
 	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		parseHTML(c, before, after)
-	}
-
-	if after != nil {
-		after(n)
+		parseHTML(c, before)
 	}
 }
