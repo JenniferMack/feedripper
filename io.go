97af2810b621ebf6d2254f977bebe703a84824df
Jennifer <JenniferMack@users.noreply.github.com>
2019-06-14 16:38:29 -0700

    add http fetcher

--- io.go
+++ io.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net/http"
+	"strings"
 )
 
 func ReadConfig(file string) (*Config, error) {
@@ -22,4 +24,35 @@ func ReadConfig(file string) (*Config, error) {
 		return nil, fmt.Errorf("tag priority out of range")
 	}
 	return &c, nil
+}
+
+func FetchItem(url, typ string) ([]byte, error) {
+	resp, err := http.Head(url)
+	if err != nil {
+		resp.Body.Close()
+		return nil, fmt.Errorf("http head: %s", err)
+	}
+
+	if resp.StatusCode >= 400 {
+		resp.Body.Close()
+		return nil, fmt.Errorf("status: %d", resp.StatusCode)
+	}
+
+	ht := resp.Header.Get("Content-Type")
+	if !strings.Contains(ht, typ) {
+		resp.Body.Close()
+		return nil, fmt.Errorf("content-type: %s", ht)
+	}
+
+	resp, err = http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("http get: %s", err)
+	}
+	defer resp.Body.Close()
+
+	b, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("body read: %s", err)
+	}
+	return b, nil
 }
---
+++ io_test.go
@@ -0,0 +1,52 @@
+package feedpub
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestFetch(t *testing.T) {
+	s := httptest.NewServer(http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("content-type", "application/json")
+			fmt.Fprint(w, `{"foo":"bar"}`)
+		}))
+
+	b, e := FetchItem(s.URL, "json")
+	if e != nil {
+		t.Error(e)
+	}
+
+	var i interface{}
+	e = json.Unmarshal(b, &i)
+	if e != nil {
+		t.Error(e)
+	}
+}
+
+func Test404(t *testing.T) {
+	s := httptest.NewServer(http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			http.NotFound(w, r)
+		}))
+
+	_, e := FetchItem(s.URL, "json")
+	if e.Error() != "status: 404" {
+		t.Error(e)
+	}
+}
+
+func TestCT(t *testing.T) {
+	s := httptest.NewServer(http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprint(w, `{"foo":"bar"}`)
+		}))
+
+	_, e := FetchItem(s.URL, "json")
+	if e.Error() != "content-type: text/plain; charset=utf-8" {
+		t.Error(e)
+	}
+}
